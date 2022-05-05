ed2c70715e69ae4ac90bf7f795bd9e705563e987
zp3tech <zp3tech@gmail.com>
2022-05-05 12:44:06 -0700

    highlight col complete
    still todo: checkBoard function to see if there's a winner.

--- CONNECT-4/app.js
+++ CONNECT-4/app.js
@@ -1,64 +1,84 @@
 document.addEventListener('DOMContentLoaded', () => {
     const boardWidth = 7
     const boardHeight = 6
-    
+
     // create grid div
     const masterGrid = document.createElement('div')
     masterGrid.classList.add('master-grid')
     document.body.appendChild(masterGrid)
 
     // creates divs within grid
-    for (let i = 0; i < (boardWidth*boardHeight); i++) {
+    for (let i = 0; i < (boardWidth * boardHeight); i++) {
         const newDiv = document.createElement('div')
+        newDiv.classList.add('col-' + (i % boardWidth))
         masterGrid.appendChild(newDiv)
     }
 
     const squares = document.querySelectorAll('.master-grid div')
-    const result = document.querySelector('#result')
+    const displayWinner = document.querySelector('#winner')
     const displayCurrentPlayer = document.querySelector('#current-player')
-    
-    
 
     let currentPlayer = 1
 
     displayCurrentPlayer.innerHTML = currentPlayer
 
-    // create column classes in grid
-    for (let i =0; i <squares.length; i++) {
-        let colNum = i % 7
-        squares[i].classList.add('col-' + colNum)
-    }
-
-    // TODO: check for wins
+    // Check for wins
     function checkBoard() {
-        var playersTokens = document.querySelectorAll('.taken')
+        var playerOnesTokens = document.querySelectorAll('.player-one')
+        var playerTwosTokens = document.querySelectorAll('.player-two')
 
-
+        // check if p1 won
+        for (let i = 0; i < playerOnesTokens.length; i++) {
+            
+        }
 
     }
 
     for (let i = 0; i < squares.length; i++) {
-        squares[i].onclick = () => {
-            // go to lowest square in column
-            if (squares[i + 7].classList.contains('taken')) {
-                if (currentPlayer == 1) {
-                    squares[i].classList.add('taken')
-                    squares[i].classList.add('player-one')
-                    currentPlayer = 2
-                    displayCurrentPlayer.innerHTML = currentPlayer
-                } else if (currentPlayer == 2) {
-                    squares[i].classList.add('taken')
-                    squares[i].classList.add('player-two')
-                    currentPlayer = 1
-                    displayCurrentPlayer.innerHTML = currentPlayer
-                }
-            } else alert('cannot play here')
         
-            checkBoard()    // check for win
+        // highlights col mouse pointer is over
+        squares[i].addEventListener('mouseover', () => {
+            colNum = squares[i].classList[0]
+            sqInCol = document.querySelectorAll('.' + colNum)
+            sqInCol.forEach(box => {
+                if (box.classList.contains('player-one')) return
+                if (box.classList.contains('player-two')) return
+                box.classList.add('highlighted-col')
+            });
+        })
+        squares[i].addEventListener('mouseout', () => {
+            sqInCol.forEach(box => {
+                box.classList.remove('highlighted-col')
+            })
+        })
+
+        squares[i].onclick = () => {
+            colNum = squares[i].classList[0]
+            sqInCol = document.querySelectorAll('.' + colNum)
+
+            for (let j = (boardHeight - 1); j >= 0; j--) {
+                if (sqInCol[j].classList.contains('player-one')) continue
+                if (sqInCol[j].classList.contains('player-two')) continue
+
+                switch (currentPlayer) {
+                    case 1:
+                        sqInCol[j].classList.add('player-one')
+                        sqInCol[j].classList.remove('highlighted-col')
+                        currentPlayer = 2
+                        displayCurrentPlayer.innerHTML = currentPlayer
+                        break
+                    case 2:
+                        sqInCol[j].classList.add('player-two')
+                        sqInCol[j].classList.remove('highlighted-col')
+                        currentPlayer = 1
+                        displayCurrentPlayer.innerHTML = currentPlayer
+                        break
+                }
+                break
+            }
+            checkBoard()
         }
     }
-
-
 })
 
 
