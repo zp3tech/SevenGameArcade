cfb8efb843e0569f8b59f0359b3b0ee9c5ff17f8
zp3tech <zp3tech@gmail.com>
2022-05-05 15:32:02 -0700

    vertical win check complete
    in progress for diagonal win check,
    still todo: diagonal win check

--- CONNECT-4/app.js
+++ CONNECT-4/app.js
@@ -19,23 +19,69 @@ document.addEventListener('DOMContentLoaded', () => {
     const displayCurrentPlayer = document.querySelector('#current-player')
 
     let currentPlayer = 1
+    let prevPlayer = 1
 
-    displayCurrentPlayer.innerHTML = currentPlayer
+    displayCurrentPlayer.innerHTML = currentPlayer + ' (red)'
 
-    // Check for wins
-    function checkBoard() {
-        var playerOnesTokens = document.querySelectorAll('.player-one')
-        var playerTwosTokens = document.querySelectorAll('.player-two')
+    let counter
 
-        // check if p1 won
-        for (let i = 0; i < playerOnesTokens.length; i++) {
-            
+    // checks for vertical wins
+    function checkVertical(index, playerString) {
+        if (index + boardWidth > (boardWidth*boardHeight -1)) {   // in bottom row
+            return counter
+        } else if (squares[index + boardWidth].classList.contains(playerString)) {
+            counter++
+            checkVertical(index + boardWidth, playerString)
+        } else {
+            return counter
         }
+    }
+
+    // checks horizontal wins
+    function checkHorizontal(index, playerString) {
+        // start checking for wins on far left column
+        index -= index % boardWidth
+        for (let i = 0; i < boardWidth; i++) {
+            
+        } 
+    }
+
+    // runs all directions to check for win
+    function checkBoard(index, pNum) {
+        let playerString = ''
+        if (pNum == 1) {
+            playerString = 'player-one'
+        } else if (pNum == 2) {
+            playerString = 'player-two'
+        }
+
+        counter = 1
+        checkVertical(index, playerString)
+        checkWin(pNum)
+
+        // checks for left/right wins
+        counter = 1
+        checkHorizontal(index, playerString)
+        checkWin(pNum)
+        
+        // checks for diagonal wins
+        // counter = 1 
+        // checkDiagonal(index, playerString)
+        // checkWin(pNum)
 
     }
 
+    function checkWin(pNum) {
+        if (counter == 4) {
+            color = ' (red)'
+            if (pNum == 2) color = ' (black)'
+            displayWinner.textContent = 'Player ' + pNum + color
+            // TODO: end/stop the game
+        }
+    }
+
     for (let i = 0; i < squares.length; i++) {
-        
+
         // highlights col mouse pointer is over
         squares[i].addEventListener('mouseover', () => {
             colNum = squares[i].classList[0]
@@ -55,6 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[i].onclick = () => {
             colNum = squares[i].classList[0]
             sqInCol = document.querySelectorAll('.' + colNum)
+            prevPlayer = currentPlayer
 
             for (let j = (boardHeight - 1); j >= 0; j--) {
                 if (sqInCol[j].classList.contains('player-one')) continue
@@ -63,20 +110,30 @@ document.addEventListener('DOMContentLoaded', () => {
                 switch (currentPlayer) {
                     case 1:
                         sqInCol[j].classList.add('player-one')
+                        sqInCol[j].classList.add('token')
                         sqInCol[j].classList.remove('highlighted-col')
                         currentPlayer = 2
-                        displayCurrentPlayer.innerHTML = currentPlayer
+                        displayCurrentPlayer.innerHTML = currentPlayer + ' (black)'
                         break
                     case 2:
                         sqInCol[j].classList.add('player-two')
+                        sqInCol[j].classList.add('token')
                         sqInCol[j].classList.remove('highlighted-col')
                         currentPlayer = 1
-                        displayCurrentPlayer.innerHTML = currentPlayer
+                        displayCurrentPlayer.innerHTML = currentPlayer + ' (red)'
                         break
                 }
                 break
             }
-            checkBoard()
+
+            // gets index of token just placed
+            for (let k =0; k < squares.length; k++) {
+                if (squares[k].classList.contains('token')) {
+                    index = k
+                    squares[k].classList.remove('token')
+                }
+            }
+            checkBoard(index, prevPlayer)
         }
     }
 })
