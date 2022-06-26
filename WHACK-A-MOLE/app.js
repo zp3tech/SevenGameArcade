d3bdf18829f75d7facaf8afc62232ab5ff86c290
Zachary Pahl <zp3tech@gmail.com>
2022-06-25 17:49:15 -0700

    restart button and reorg

--- WHACK-A-MOLE/app.js
+++ WHACK-A-MOLE/app.js
@@ -1,51 +1,80 @@
 const squares = document.querySelectorAll('.square')
 const mole = document.querySelector('.mole')
 const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
+const hitsDisplay = document.querySelector('#hits')
+const missesDisplay = document.querySelector('#misses')
+const accuDisplay = document.querySelector('#accuracy')
+const restartButton = document.querySelector('#restart-btn')
 
-let result = 0
+const TIME_LENGTH = 10
+
+restartButton.addEventListener('click', startRestart)
+timeLeft.textContent = TIME_LENGTH
+
+let currentTime = TIME_LENGTH
+let hits = 0
+let misses = 0
 let hitPosition
-let currentTime = 60
-let timerId = null
+let moveMoleTimerId = null
+let countDownTimerId = null
+// TODO: have user select speed for mole to move, 
+let moleSpeed = 350
 
 
-function randomSquare() {
+function startRestart() {
+  // reset timer
+  clearInterval(countDownTimerId)
+  currentTime = TIME_LENGTH
+
+  // restart timer
+  countDownTimerId = setInterval(countDown, 1000)
+  timeLeft.textContent = currentTime
+
+  // assign hits/misses for each square in grid
   squares.forEach(square => {
-    square.classList.remove('mole')
+    square.addEventListener('mousedown', respondToClick)
   })
 
-  let randomSquare = squares[Math.floor(Math.random() * 9)]
-  randomSquare.classList.add('mole')
-
-  hitPosition = randomSquare.id
+  moveMole()
 }
 
-squares.forEach(square => {
-  square.addEventListener('mousedown', () => {
-    if (square.id == hitPosition) {
-      result++
-      score.textContent = result
-      hitPosition = null
-    }
-  })
-})
-
-function moveMole() {
-  timerId = setInterval(randomSquare, 400)
+function respondToClick() {
+  if (this.id == hitPosition) {
+    hits++
+    hitsDisplay.textContent = hits  
+  } else {
+    misses++
+    missesDisplay.textContent = misses
+  }
+  accuDisplay.textContent = hits / (hits + misses) * 100
 }
 
-moveMole()
-
+// round timer
 function countDown() {
   currentTime--
   timeLeft.textContent = currentTime
-
   if (currentTime == 0) {
     clearInterval(countDownTimerId)
-    clearInterval(timerId)
-    alert('Game over! Your final score is ' + result)
+    clearTimeout(moveMoleTimerId)
+
+    squares.forEach(square => {
+      square.removeEventListener('mousedown', respondToClick)
+    })
   }
 }
 
-let countDownTimerId = setInterval(countDown, 1000)
+// move mole randomly around grid
+function moveMole() {
+  squares.forEach(square => {
+    square.classList.remove('mole')
+  })
+  let randomSquare = squares[Math.floor(Math.random() * 9)]
+  randomSquare.classList.add('mole')
+  hitPosition = randomSquare.id
 
+  //set random delay to stutter mole movement, no more than half of moleSpeed
+  let delay = Math.floor(Math.random() * (moleSpeed / 2))
+  moveMoleTimerId = setTimeout(moveMole, (delay + moleSpeed))
+}
+
+// TODO: hitmarker animation on hit
